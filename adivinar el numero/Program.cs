7ccdfa9d0f8738175395e8bdcd67cf5676b6af00
alumno <alumno@UMG-VM>
2021-03-05 23:06:45 -0600

    hola

---
+++ adivinar el numero/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace adivinar_el_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;
+
+            Console.WriteLine("anote un numero random press enter para continua");
+                Console.WriteLine("multiplique ese número x2 press enter para continuar");
+            
+            Console.WriteLine("suma 8 al resultado press enter para continuar" );
+           
+            Console.WriteLine("Multiplica x5 press enter para continuar");
+
+            Console.WriteLine("ingrese el total final");
+
+            string x = (Console.ReadLine());
+            x = x.Remove(x.Length - 1);
+            numero = Convert.ToInt32(x);
+
+
+            numero = numero - 4;
+            Console.WriteLine("el numero que pensaste es: " + numero);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
