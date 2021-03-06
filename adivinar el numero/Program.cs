30b751dc250e34f81f9ea3d7d0e73e8450f1b70c
alumno <alumno@UMG-VM>
2021-03-05 23:47:30 -0600

    final

--- adivinar el numero/Program.cs
+++ adivinar el numero/Program.cs
@@ -7,7 +7,14 @@ namespace adivinar_el_numero
         static void Main(string[] args)
         {
 
-            int numero;
+            int opcioon =00;
+
+            Console.WriteLine("ingrese la opcion      1= adivinar numero     2= adivinar edades");
+            opcioon = Convert.ToInt32(Console.ReadLine());
+
+            if (opcioon == 1)
+            {
+                int numero;
 
             Console.WriteLine("anote un numero random press enter para continua");
                 Console.WriteLine("multiplique ese número x2 press enter para continuar");
@@ -27,11 +34,29 @@ namespace adivinar_el_numero
             Console.WriteLine("el numero que pensaste es: " + numero);
 
             Console.ReadKey();
+            }
+            else if (opcioon == 2)
+
+            {
+
+
+                Console.WriteLine("Anote el dia en que nacio press enter para continuar ");
+                Console.WriteLine("Multiplique x20 el dato press enter para continuar");
+                Console.WriteLine("Ahora sumale 73 press enter para continuar");
+                Console.WriteLine("Ahora multiplicalo x5 press enter para continuar");
+                Console.WriteLine("Sumale el numero del mes de cumpleaños press enter para continuar");
+                Console.WriteLine("ingrese el total final");
+
+                int numero = Convert.ToInt32(Console.ReadLine());
+                numero = numero - 365;
+                String a = Convert.ToString(numero);
+                Console.WriteLine("El dia en que nacio es" + a.Substring(0, 2) + "el mes en que nacio es" + a.Substring(2, 2));
+
+                Console.ReadKey();
 
 
 
-
-
+            }
         }
     }
 }
